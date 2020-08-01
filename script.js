9db9c4fa142e511bb1c3a231e0a14105e63e2d60
ananrt2517 <stepanovicana6@gmail.com>
2020-08-01 12:22:55 +0200

    calc age

---
+++ script.js
@@ -0,0 +1,25 @@
+// skripta mora da se pokrene pomocu noda u consoli nikako u brow
+// Napisati metodu koja za date podatke vraca listu ljudi koji su stariji od 20 godina (lista objekata, kao i u json-u)
+// Metoda mora da ima kao ulazni parametar funkciju koja izracunava godine (moze da ima i druge parametre, bitno je samo da je jedan od njih funkcija);
+
+const ljudi = require("./ljudi.json");
+
+function vratiListuLjudi(fn) {
+  let novaListaLjudi = [];
+  for (let user of ljudi) {
+    let funkc = fn(user.godiste);
+    if (funkc > 20) {
+      novaListaLjudi.push(user);
+    }
+  }
+
+  return novaListaLjudi;
+}
+
+const listaStarijihOd20 = vratiListuLjudi(izrGodine);
+const listaStarijihOd40 = vratiListuLjudi(izrGodine, 40);
+
+
+function izrGodine(godRodjenja) {
+  return 2020 - godRodjenja;
+}
