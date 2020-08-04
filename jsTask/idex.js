ccd08c8fc5ee1245dd0c38cd2a50d2e67a6c30e5
mladjanupconfig <mladjan@upconfig.com>
2020-08-04 08:53:59 +0200

    find minimum

--- jsTask/idex.js
+++ jsTask/idex.js
@@ -10,20 +10,52 @@
 
 //zadatak2
 
-let array = [3,1,'afs', 'vre', 4];
+// let array = [3,1,'afs', 'vre', 4, -2, 5, 17];
 
-function findNumber(arr){
+// function findNumber(arr){
+//     let newArray = [];
+//     for(let i = 0; i < arr.length; i++){
+//         if(typeof arr[i] === 'number'){
+//             // console.log(arr[i])
+//             newArray.push(arr[i]);
+//         }
+//     }
+//     newArray.sort(function(a, b){return a-b});
+//     let min = newArray[0];
+//     return min;  
+// }
+// let funkc = findNumber(array);
+// console.log(funkc)
+
+//zadatak 3
+
+let minString = 'Danas, 15tog Septembra, bice hladnije nego juce u 20h, 16og Septembra';
+
+// function minIntegerFromString(str){
+//     let regex = /\d+/g;
+//     let matches = str.match(regex);
+//     let sort = matches.sort();
+//     let min =parseInt(sort)
+//     console.log(min);
+//     return min
+    
+
+// }
+// let minimum = minIntegerFromString(minString)
+// console.log(minimum)
+
+function minIntegerFromString(str){
     let newArray = [];
-    for(let i = 0; i<arr.length; i++){
-        if(Number.isInteger(arr[i])){
-            console.log(arr[i])
-            // newArray.push(arr[i]);
+    let splitStr = str.split("");    
+    for(let i = 0; i < splitStr.length ; i ++){
+        let parse = parseInt(splitStr[i]);
+        if(Number.isInteger(parse)){
+            newArray.push(parse);
         }
     }
     newArray.sort(function(a, b){return a-b});
     let min = newArray[0];
-    // console.log(min);
-   
+    return min; 
 }
-let funkc = findNumber(array);
-console.log(funkc)
+let minimum = minIntegerFromString(minString)
+console.log(minimum)
