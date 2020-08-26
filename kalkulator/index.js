38ac6c72f16821e5f195332a5785e49e189a49e5
ananrt2517 <stepanovicana6@gmail.com>
2020-08-26 21:14:53 +0200

    clear console.log

--- kalkulator/index.js
+++ kalkulator/index.js
@@ -5,17 +5,13 @@ function pushBtn(obj) {
 
   if (pushedBtn == "=") {
     result.innerHTML = eval(result.innerHTML);
-    console.log(result.innerHTML)
   } else if (pushedBtn == "AC") {
     result.innerHTML = "";
-    console.log(result)
   } else {
     if (pushedBtn == "0") {
       result.innerHTML += pushedBtn;
-      console.log(result)
     } else {
       result.innerHTML += pushedBtn;
-      console.log(result.innerHTML)
     }
   }
 }
