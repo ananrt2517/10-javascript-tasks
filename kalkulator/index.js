22273de27390bf39941d5827be644faa7ad38de8
ananrt2517 <stepanovicana6@gmail.com>
2020-08-26 21:12:44 +0200

    fix all clear button

--- kalkulator/index.js
+++ kalkulator/index.js
@@ -1,26 +1,20 @@
 let result = document.querySelector(".result");
-// console.log(result.innerHTML)
 
 function pushBtn(obj) {
   let pushedBtn = obj.innerText;
-  // console.log(pushedBtn)
 
   if (pushedBtn == "=") {
     result.innerHTML = eval(result.innerHTML);
-    // return result;
     console.log(result.innerHTML)
   } else if (pushedBtn == "AC") {
-    result.innerHTML = "0";
-    // return result;
+    result.innerHTML = "";
     console.log(result)
   } else {
     if (pushedBtn == "0") {
-      result.innerHTML = pushedBtn;
+      result.innerHTML += pushedBtn;
       console.log(result)
-      // return result;
     } else {
       result.innerHTML += pushedBtn;
-      // return result;
       console.log(result.innerHTML)
     }
   }
