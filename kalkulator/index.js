aeb91dc9a17a6b9c039a71045f2a4dc1bf996f35
ananrt2517 <stepanovicana6@gmail.com>
2020-08-27 21:22:38 +0200

    fix bags

--- kalkulator/index.js
+++ kalkulator/index.js
@@ -2,9 +2,26 @@ let result = document.querySelector(".result");
 
 function pushBtn(obj) {
   let pushedBtn = obj.innerText;
+  let operations = [".", "+", "-", "/", "*"];
+
+  if (operations.indexOf(pushedBtn) != -1) {
+    for (let i = 0; i < operations.length; i++) {
+      if (result.innerText.includes(operations[i])) return;
+    }
+  }
+
+  for (let item of operations) {
+    if (result.innerHTML == "" && pushedBtn == item) {
+      return;
+    }
+  }
 
   if (pushedBtn == "=") {
-    result.innerHTML = eval(result.innerHTML);
+    if (result.innerHTML != "") {
+      result.innerHTML = eval(result.innerHTML);
+    } else {
+      return;
+    }
   } else if (pushedBtn == "AC") {
     result.innerHTML = "";
   } else {
